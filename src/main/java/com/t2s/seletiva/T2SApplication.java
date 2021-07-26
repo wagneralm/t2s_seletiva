9f7c45b1d5f35ce62435ecbe877155fc7b493d25
Wagner de Almeida <wagneer.alm@gmail.com>
2021-07-25 23:58:07 -0300

    first commit

---
+++ src/main/java/com/t2s/seletiva/T2SApplication.java
@@ -0,0 +1,13 @@
+package com.t2s.seletiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2SApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t2s/seletiva/T2SApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t2s.seletiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2SApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
